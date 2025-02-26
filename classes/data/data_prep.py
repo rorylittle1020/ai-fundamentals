23657115b6f95355200910618df8d7eb32729e70
rorylittle1020 <rory.little1020@gmail.com>
2025-02-26 09:54:19 -0500

    New Feature

--- classes/data/data_prep.py
+++ classes/data/data_prep.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 
 df = pd.DataFrame({"A": [1, 2, 3], "B": [1, 2, 3]})
 
-df2 = pd.DataFrame({"C": [1, 2, 3], "D": [1, 2, 3]})
+# New Feature
+
+df2 = pd.DataFrame({"C": [1, 2, 3, 4], "D": [1, 2, 3, 4]})
 
 df3 = pd.DataFrame({"E": [1, 2, 3], "F": [1, 2, 3]})
