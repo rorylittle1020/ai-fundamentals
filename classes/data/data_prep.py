af50313e50884ed4eae9375fba7812fc08201606
rorylittle1020 <rory.little1020@gmail.com>
2025-02-26 09:42:28 -0500

    Added a third dataframe as a test

--- classes/data/data_prep.py
+++ classes/data/data_prep.py
@@ -5,3 +5,5 @@ import matplotlib.pyplot as plt
 df = pd.DataFrame({"A": [1, 2, 3], "B": [1, 2, 3]})
 
 df2 = pd.DataFrame({"C": [1, 2, 3], "D": [1, 2, 3]})
+
+df3 = pd.DataFrame({"E": [1, 2, 3], "F": [1, 2, 3]})
